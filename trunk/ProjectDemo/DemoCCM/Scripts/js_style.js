ad571ef76ffa7ee872c70d07af877382ea7d7ce5
hongphongcntt@gmail.com <hongphongcntt@gmail.com@8b59389d-0a69-5c92-a5de-98b60eff5180>
2013-11-17 15:00:37 +0000

    update drag & drop , add item , remove item

--- trunk/ProjectDemo/DemoCCM/Scripts/js_style.js
+++ trunk/ProjectDemo/DemoCCM/Scripts/js_style.js
@@ -1,10 +1,67 @@
 ﻿
 $(document).ready(function ()
 {
-    $("#updow1").click(function(){
-        $("div.boxhide").toggle();
-    });
-    $("#updow2").click(function () {
-        $("div.boxhide2").toggle();
+    $(function () {
+        var $gallery = $("#gallery"),
+          $trash = $("#trash");
+
+        $("li", $gallery).draggable({
+            cancel: "a.ui-icon",
+            revert: "invalid",
+            containment: "document",
+            helper: "clone",
+            cursor: "move"
+        });
+
+        $trash.droppable({
+            accept: "#gallery > li",
+            drop: function (event, ui) {
+                addItem(ui.draggable);
+            }
+        });
+
+        $gallery.droppable({
+            accept: "#trash li",
+            //activeClass: "custom-state-active",
+            drop: function (event, ui) {
+                removeItem(ui.draggable);
+            }
+        });
+
+        var remove_item_icon = "<a title='Remove item' class='ui-icon ui-icon-minus'>-</a>";
+        function addItem($item) {
+            $item.fadeOut(1, function () {
+                var $list = $("ul", $trash).length ?
+                  $("ul", $trash) :
+                  $("<ul class='gallery ui-helper-reset'/>").appendTo($trash);
+                $item.find("a.ui-icon-plus").remove();
+                $item.append(remove_item_icon).appendTo($list).fadeIn(function () {
+                    //$item.css({ "width": "100px","border":"2px solid orange","border-radius":"5px","background-color":"orange","color":"white"})
+                });
+            });
+        }
+        var add_item_icon = "<a title='Add this Item' class='ui-icon ui-icon-plus'>+</a>";
+        function removeItem($item) {
+            $item.fadeOut(1, function () {
+                $item
+                  .find("a.ui-icon-minus")
+                  .remove()
+                  .end()
+                   //.css({ "width": "140px", "height": "", "border": "1px dashed orange" })
+                  .append(add_item_icon)
+                  .appendTo($gallery)
+                  .fadeIn();
+            });
+        }
+        $("ul.gallery > li").click(function (event) {
+            var $item = $(this),
+              $target = $(event.target);
+            if ($target.is("a.ui-icon-plus")) {
+                addItem($item);
+            } else if ($target.is("a.ui-icon-minus")) {
+                removeItem($item);
+            }
+            return false;
+        });
     });
 });
